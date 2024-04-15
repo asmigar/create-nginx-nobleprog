155cf6936380c5329a8110a4e8ae306f813caf15
Sagar Maurya <147434812+asmigar@users.noreply.github.com>
2024-04-15 18:19:11 +0530

    use lb

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -25,12 +25,11 @@ resource "aws_security_group" "allow_ssh" {
   }
 
   ingress {
-    description = "nginx"
+    description = "lb"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    security_groups = [aws_security_group.lb_sg.id]
   }
 
   egress {
