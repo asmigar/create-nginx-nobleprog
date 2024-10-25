56d6451bd953cc7c3f7e0ba883b5288c5c069988
Sagar Maurya <147434812+asmigar@users.noreply.github.com>
2024-10-25 15:24:49 +0530

    messup tf format

--- infra/app/asg.tf
+++ infra/app/asg.tf
@@ -2,7 +2,7 @@ resource "aws_autoscaling_policy" "scale_up_nginx" {
   name                   = module.nobleprog_alpha.id
   autoscaling_group_name = aws_autoscaling_group.nginx.name
   adjustment_type        = "ChangeInCapacity"
-  scaling_adjustment     = 1
+  scaling_adjustment  = 1
   cooldown               = 120
 }
 
